f9ce8d5385ecce70efc38ef57962e6c576055614
Skylar <mastersliebenberg@gmail.com>
2026-03-11 13:53:13 +0200

    Created PlaceSearchBST to search binary search tree and incorporate the operations set out in PlaceNameBST

---
+++ CSC2001F_Assignment_1/src/PlaceSearchBST.java
@@ -0,0 +1,69 @@
+import java.util.Scanner;
+
+public class PlaceSearchBST {
+    public static void main(String[] args) {
+        PlaceNameBST PNB = new PlaceNameBST();
+        System.out.println("Welcome to the PlaceSearchArray Terminal Dialogue");
+        Scanner scanner = new Scanner(System.in);
+        boolean bQuit = false;
+        while (!bQuit) {
+            System.out.println("What would you like to do? Press (q) to quit the application.");
+            System.out.println("1. Load data from a file into the BST.");
+            System.out.println("2. Search BST for entry matching a place name.");
+            System.out.println("3. Delete from BST.");
+            System.out.println("4. Return number of comparisons for most recent search.");
+            System.out.println("5. Tree height of BST.");
+            switch (scanner.next().toLowerCase().charAt(0)) {
+                case '1':
+                    scanner.nextLine(); //gets rid of \n from reading first char, allowing user to say 1 or 1. and q or quit
+                    System.out.println("What is the name of the file?");
+                    String fileName = scanner.nextLine();
+                    System.out.println("How many lines of the array would you like to load?");
+                    int N = scanner.nextInt();
+                    System.out.println("You are loading " + N + " lines, from the file \"" + fileName + "\". \nConfirm [Y/N]:");
+                    if (scanner.next().toLowerCase().charAt(0) == 'y') {
+                        PNB.loadBST(N, fileName);
+                        System.out.println("Array loaded successfully.");
+                    }
+                    break;
+                case '2':
+                    scanner.nextLine();
+                    System.out.println("What is the name of the place you are looking for?");
+                    String placeName = scanner.nextLine();
+                    System.out.println("You are looking for place \"" + placeName + "\". \nConfirm [Y/N]:");
+                    if (scanner.next().toLowerCase().charAt(0) == 'y') { //allows user to say Y, Yes, y or yes
+                        PlaceNameEntry search = PNB.searchTree(placeName);
+                        if (search.placeName().isEmpty()){
+                            System.out.println("The place could not be found.");
+                        }
+                        else {
+                            System.out.println("The place " + search.placeName() + " has ID " + search.id() + ". It is in " + search.municipality() + ", " + search.province() + ". It has a population of " + search.population() + ".");
+                        }
+                    }
+                    break;
+                case '3':
+                    scanner.nextLine();
+                    System.out.println("What is the name of the place corresponding to the desired deletion?");
+                    String deletion = scanner.nextLine();
+                    System.out.println("You are about to delete the record with place name " + deletion + ". Confirm [Y/N]:");
+                    if (scanner.next().toLowerCase().charAt(0) == 'y') { //allows user to say Y, Yes, y or yes
+                        PNB.deleteBST(deletion);
+                        System.out.println("The record has been deleted.");
+                        }
+                    break;
+                case '4':
+                    scanner.nextLine();
+                    System.out.println("There were " + PNB.getComparisons() + " comparisons in your most recent search.");
+                    break;
+                case '5':
+                    scanner.nextLine();
+                    System.out.println("The tree has a height (longest path from first root to last leaf) of " + PNB.getTreeHeight() + ".");
+                case 'q':
+                    System.out.println("Thank you");
+                    bQuit = true;
+                    break;
+            }
+           }
+
+    }
+}
