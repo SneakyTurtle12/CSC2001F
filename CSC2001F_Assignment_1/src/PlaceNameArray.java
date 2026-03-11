cd6907148a63d972a0f5669c4be817cb445aefde
Skylar <mastersliebenberg@gmail.com>
2026-03-11 12:07:12 +0200

    Created PlaceSearchArray, allowing a user to search through an array loaded from the file sequentially. SAPlaceNames.csv contains the files to be read and PlaceNameArray was altered to fix a small bug.

--- CSC2001F_Assignment_1/src/PlaceNameArray.java
+++ CSC2001F_Assignment_1/src/PlaceNameArray.java
@@ -5,16 +5,17 @@ import java.util.Scanner;
 import java.util.Set;
 
 public class PlaceNameArray {
+    PlaceNameEntry[] arrPlaces;
     private int comparisons;
     private int iCount = 0;
     public void loadArray(int size, String fileName){
         Set<String> seen = new HashSet<>();
         File cFile = new File(fileName);
-        PlaceNameEntry[] arrPlaces;
         arrPlaces = new PlaceNameEntry[size];
         try{
             Scanner scanner = new Scanner(cFile);
-            while (scanner.hasNextLine() && iCount <= size){
+            scanner.nextLine();
+            while (scanner.hasNextLine() && iCount < size){
                 String line = scanner.nextLine();
                 String[] values = line.split(",");
                 if (seen.add(values[1])) //O(1) complexity on hashset duplicate checks
@@ -22,13 +23,14 @@ public class PlaceNameArray {
                     arrPlaces[iCount] = new PlaceNameEntry(values[0], values[1], values[2], values[3], Integer.valueOf(values[4]));
                     iCount +=1;
                 }
-                scanner.close();
+
             }
+            scanner.close();
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
     }
-    public PlaceNameEntry Search(PlaceNameEntry[] arrPlaces, String name){
+    public PlaceNameEntry search(String name){
         comparisons = 0;
         for (int i = 0; i < iCount; i++){
             comparisons+=1;
---
+++ CSC2001F_Assignment_1/src/PlaceSearchArray.java
@@ -0,0 +1,55 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class PlaceSearchArray {
+    public static void main(String[] args){
+        PlaceNameArray PNA = new PlaceNameArray();
+        System.out.println("Welcome to the PlaceSearchArray Terminal Dialogue");
+        Scanner scanner = new Scanner(System.in);
+        boolean bQuit = false;
+        while (!bQuit) {
+            System.out.println("What would you like to do? Press (q) to quit the application");
+            System.out.println("1. Load data from a file into the array");
+            System.out.println("2. Search array for entry matching a place name");
+            System.out.println("3. Return number of comparisons for most recent search");
+            switch (scanner.next().toLowerCase().charAt(0)){
+                case '1':
+                    scanner.nextLine(); //gets rid of \n from reading first char, allowing user to say 1 or 1. and q or quit
+                    System.out.println("What is the name of the file?");
+                    String fileName = scanner.nextLine();
+                    System.out.println("How many lines of the array would you like to load?");
+                    int N = scanner.nextInt();
+                    System.out.println("You are loading " + N + " lines, from the file \"" + fileName + "\". \nConfirm [Y/N]:");
+                        if (scanner.next().toLowerCase().charAt(0) == 'y') {
+                                PNA.loadArray(N, fileName);
+                                System.out.println("Array loaded successfully.");
+                    }
+                    break;
+                case '2':
+                    scanner.nextLine();
+                    System.out.println("What is the name of the place you are looking for?");
+                    String placeName = scanner.nextLine();
+                    System.out.println("You are looking for place \"" + placeName + "\". \nConfirm [Y/N]:");
+                    if (scanner.next().toLowerCase().charAt(0) == 'y') { //allows user to say Y, Yes, y or yes
+                        PlaceNameEntry search = PNA.search(placeName);
+                        if (search.placeName().isEmpty()){
+                            System.out.println("The place could not be found.");
+                        }
+                        else {
+                            System.out.println("The place " + search.placeName() + " has ID " + search.id() + ". It is in " + search.municipality() + ", " + search.province() + ". It has a population of " + search.population() + ".");
+                        }
+                    }
+                    break;
+                case '3':
+                    scanner.nextLine(); //no need to confirm here, there is no other input and it merely fetches a variable
+                    System.out.println("There were " + PNA.getComparisons() + " comparisons in your most recent search.");
+                    break;
+                case 'q':
+                    bQuit = true;
+                    break;
+            }
+        }
+
+    }
+}
